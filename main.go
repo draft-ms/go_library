041e11ae658aa6d4fd555a04d821b3483351e842
Minsu Park <draftms@gmail.com>
2021-03-12 17:58:04 +0900

    add mongoDB Connection

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"time"
+
 	"github.com/draftms/go_library/logging"
 	"github.com/kardianos/service"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type program struct{}
 
 var logger = logging.NewInstance()
+
+type user struct {
+/*     _id string
+    email string
+    password string
+    name string
+    role string
+    hospitalid string
+    status string
+    hospitals string */
+}
 
 func (p *program) Start(s service.Service) error {
 	// Start should not block. Do the actual work async.
@@ -31,7 +49,8 @@ func (p *program) Stop(s service.Service) error {
 var servicLoger service.Logger
 
 func main() {
-
+    //1. for windows service
+/* 
  	svcConfig := &service.Config{
 		Name:        "GoSVCTest",
 		DisplayName: "Go SVC Test",
@@ -51,4 +70,47 @@ func main() {
 	if err != nil {
 		servicLoger.Error(err)
 	}
+ */
+
+/*  
+    defer func() {
+        err = client.Disconnect(context.TODO())
+
+        if err != nil {
+            log.Fatal(err)
+        }
+    }()
+*/
+
+    clientOptions := options.Client().ApplyURI("mongodb://xxx:xxx@localhost:27027/admin?safe=true")
+
+    client, err := mongo.Connect(context.TODO(), clientOptions)
+
+    if err != nil { 
+        logger.Error(err)
+    }
+
+    err = client.Ping(context.TODO(), nil)
+
+    if err != nil {
+        logger.Error(err)
+    }
+
+    collection := client.Database("cloudInfo").Collection("user")
+
+    //var result user
+
+    //filter := bson.D{{"name", "Cloud"}}
+
+    filterCursor, err := collection.Find(context.TODO(), bson.M{"name":"Cloud"})
+    if err != nil {
+        logger.Fatal(err)
+    }
+
+    var userList []bson.M
+    if err = filterCursor.All(context.TODO(), &userList); err != nil {
+        logger.Fatal(err)
+    }
+
+    fmt.Println("Connection : ", clientOptions.AppName)
 }
