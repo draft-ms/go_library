c7f1d6155142345d064dc50bb9d5ee31d44a04d2
draftms <draftms@gmail.com>
2021-03-14 19:48:35 +0900

    Add mongoDB insert,find action

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
+	"reflect"
+
+	//"encoding/json"
 
 	"github.com/draftms/go_library/logging"
 	"github.com/kardianos/service"
 
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -17,15 +21,10 @@ type program struct{}
 
 var logger = logging.NewInstance()
 
-type user struct {
-/*     _id string
-    email string
-    password string
-    name string
-    role string
-    hospitalid string
-    status string
-    hospitals string */
+type analyzer struct {
+	name string `bson:"logName"`
+	code string `bson:"logCode"`
+	cusvalue string `bson:"bson:logValue"`
 }
 
 func (p *program) Start(s service.Service) error {
@@ -82,7 +81,7 @@ func main() {
     }()
 */
 
-    clientOptions := options.Client().ApplyURI("mongodb://xxx:xxx@localhost:27027/admin?safe=true")
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/admin?safe=true")
 
     client, err := mongo.Connect(context.TODO(), clientOptions)
 
@@ -96,21 +95,49 @@ func main() {
         logger.Error(err)
     }
 
-    collection := client.Database("cloudInfo").Collection("user")
+    collection := client.Database("local").Collection("analyzer")
 
     //var result user
 
     //filter := bson.D{{"name", "Cloud"}}
 
-    filterCursor, err := collection.Find(context.TODO(), bson.M{"name":"Cloud"})
-    if err != nil {
-        logger.Fatal(err)
-    }
 
-    var userList []bson.M
-    if err = filterCursor.All(context.TODO(), &userList); err != nil {
-        logger.Fatal(err)
-    }
+	//1.Insert mongoDB data
+	insertResult, err := collection.InsertOne(context.TODO(), bson.D{
+		{Key:"name",Value:"test2"},
+		{Key:"code",Value:"10-12"},
+		{Key:"cusvalue",Value:"hud"},
+	})
 
-    fmt.Println("Connection : ", clientOptions.AppName)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+    fmt.Println("Add data : ", insertResult.InsertedID)
+
+	//2.Find mongoDB data
+	findResultCursor, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	if err = findResultCursor.All(context.TODO(), bson.D{}); err != nil {
+		fmt.Println(err)
+	}
+
+	//fmt.Println("Add data : ", findResult.ID())
+
+	for findResultCursor.Next(context.Background()) {
+		var result bson.M
+		err := findResultCursor.Decode(&result)
+
+		if err != nil {
+			fmt.Println("cursor.Next() error :",err)
+			os.Exit(1)
+		} else {
+			fmt.Println("result type :", reflect.TypeOf(result))
+			fmt.Println("result :", result)
+		}
+	}
 }
