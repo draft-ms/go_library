caaf1e1cd7a287f3ba81afe4562984e9ea3f34c5
draftms <draftms@gmail.com>
2021-03-21 12:31:55 +0900

    make expectedStudy update function

--- test/mongoDB_test.go
+++ test/mongoDB_test.go
@@ -3,15 +3,19 @@ package test
 import (
 	"context"
 	"fmt"
+	_"io/fs"
+	_"io/ioutil"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
+	_"strconv"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/draftms/go_library/logging"
 )
@@ -40,14 +44,103 @@ type ExpectedStudy struct {
 	SeriesUID string		`bson:"seC,omitempty"`
 	AddtionalBackupF string	`bson:"abf,omitempty"`
 	MRequestY string 		`bson:"mrY,omitempty"`
-	UploadInfo []UploadInfoData `bson:"uploadInfo,omitempty"`
+	UploadInfo []UploadInfoData 	`bson:"uploadInfo,omitempty"`
 }
 
 type UploadInfoData struct {
 	S3Key string			`bson:"s3key,omitempty"`
-	size string				`bson:"size,omitempty"`
+	Size int				`bson:"size,omitempty"`
 	BackupDate string		`bson:"bD,omitempty"`
-	SOPCount string			`bson:"soC,omitempty"`
+	SOPCount int			`bson:"soC,omitempty"`
+}
+
+func TestExpectedStudy(t *testing.T) {
+	//Find mongoDB data
+	mongoDBConnect()
+    collection := client.Database("Dev").Collection("ExpectedStudy")
+
+	findResultCursor, err := collection.Find(ctx, bson.D{})
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	if err = findResultCursor.All(ctx, bson.D{}); err != nil {
+		fmt.Println(err)
+	}
+
+	file, err := os.Create("modifiedUploadInfoFile.txt")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for findResultCursor.Next(ctx) {
+		var findResult ExpectedStudy
+		err := findResultCursor.Decode(&findResult)
+
+		if err != nil {
+			fmt.Println("cursor.Next() error :",err)
+			os.Exit(1)
+		} else {
+			fmt.Println("result type :", reflect.TypeOf(findResult))
+			fmt.Println("result :", findResult)
+
+			haveDCMFile := false
+
+			var prvUploadInfo UploadInfoData
+			var newUploadInfo UploadInfoData
+
+			newExpectedStudy := findResult
+			newExpectedStudy.UploadInfo = nil
+
+			for idx, uploadInfoValue := range findResult.UploadInfo {
+				if strings.Contains(uploadInfoValue.S3Key,".dcm") {
+					haveDCMFile = true
+				}
+
+				if idx == 0 {
+					prvUploadInfo = uploadInfoValue
+				} else if !haveDCMFile && idx == 1 {
+					if uploadInfoValue.S3Key == prvUploadInfo.S3Key && 
+				   	   uploadInfoValue.Size == prvUploadInfo.Size &&
+					   uploadInfoValue.BackupDate == prvUploadInfo.BackupDate {
+
+						newUploadInfo.S3Key = uploadInfoValue.S3Key
+						newUploadInfo.Size = uploadInfoValue.Size
+						newUploadInfo.BackupDate = uploadInfoValue.BackupDate
+						newUploadInfo.SOPCount = uploadInfoValue.SOPCount + prvUploadInfo.SOPCount
+
+						newExpectedStudy.UploadInfo = append(newExpectedStudy.UploadInfo, newUploadInfo)
+					   }
+				}
+			}
+
+			if !haveDCMFile {
+				//uploadInfo 를 하나로 병합
+				updateResult, err := collection.UpdateOne(
+					ctx, 
+					bson.M{"_id": findResult.ID},
+					bson.D{
+						{"$set", bson.D{{"uploadInfo",newExpectedStudy.UploadInfo[0]}}},
+					},
+				)
+	
+				if err != nil {
+					fmt.Println("UpdateErr")
+				}
+	
+				fmt.Printf("Update %v Document\n", updateResult.ModifiedCount)
+
+				//업데이트 doc를 파일로 저장
+				fmt.Fprintln(file, findResult.ID.Hex())
+			}
+		}
+	}
+
+	client.Disconnect(ctx)
 }
 
 func mongoDBConnect(){
@@ -192,43 +285,4 @@ func TestMongoDBFindOne(t *testing.T) {
 	}
 
 	client.Disconnect(ctx)
-}
-
-func TestExpectedStudy(t *testing.T) {
-		//Find mongoDB data
-		mongoDBConnect()
-		collection := client.Database("Dev").Collection("ExpectedStudy")
-	
-		findResultCursor, err := collection.Find(ctx, bson.M{})
-	
-		if err != nil {
-			logger.Fatal(err)
-		}
-	
-		if err = findResultCursor.All(ctx, bson.D{}); err != nil {
-			fmt.Println(err)
-		}
-	
-		//fmt.Println("Add data : ", findResult.ID())
-	
-		for findResultCursor.Next(ctx) {
-			var result bson.M
-			err := findResultCursor.Decode(&result)
-	
-			if err != nil {
-				fmt.Println("cursor.Next() error :",err)
-				os.Exit(1)
-			} else {
-				fmt.Println("result type :", reflect.TypeOf(result))
-				fmt.Println("result :", result)
-	
-				var expectedStudy ExpectedStudy
-				bsonBytes, _ := bson.Marshal(result)
-				bson.Unmarshal(bsonBytes, &expectedStudy)
-	
-				fmt.Println("struct data :", expectedStudy)
-			}
-		}
-	
-		client.Disconnect(ctx)
 }
